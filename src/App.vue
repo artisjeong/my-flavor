cf6d40c0e7d241b35a121c8bcd2c8f9afa78b730
Inseop Jeong <art.isjeong@gmail.com>
2021-01-04 18:27:08 +0900

    side-view and toolbar template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <Drawer :drawer="drawer" @opened="opened"/>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+    <Footer/>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
+import Footer from './components/Footer';
+import Drawer from './components/Drawer';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+    Footer,
+    Drawer,
+  },
+
+  data: () => ({
+    drawer: null
+  }),
+
+  methods: {
+    opened(isOpened) {
+      this.drawer = isOpened;
+      console.log("열림>>>  " + isOpened);
+    }
+  }
+};
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-bottom-navigation v-model="index" color="primary" grow>
+        <v-btn>
+            <v-icon>mdi-home</v-icon>
+        </v-btn>
+
+        <v-btn>
+            <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn>
+            <v-icon>mdi-map-marker</v-icon>
+        </v-btn>
+        
+        <v-btn>
+            <v-icon>mdi-account</v-icon>
+        </v-btn>
+    </v-bottom-navigation>
+</template>
+<script>
+export default {
+    name: 'Footer',
+    data: () => ({ index: 0 }),
+}
+</script>
